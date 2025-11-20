99ed52a9bcc1e7ae8fdb2ba79ffb4ecc6f6d8c08
MiniOcean404 <1037306928@qq.com>
2025-11-20 23:27:21 +0800

    ✨ feat: 热键

--- src/pancel/index.ts
+++ src/pancel/index.ts
@@ -25,20 +25,40 @@ export function onMountedHotkeys(
   let isMoutend = false
   let root: ReactDOM.Root | null = null
 
-  hotkeys(keys, function (event, handler) {
-    console.log("✅ ~ hotkeys - :", event, handler, isMoutend)
+  // 激活 scope 作用域
+  hotkeys.setScope("global")
 
-    event.preventDefault()
+  hotkeys(
+    keys,
+    {
+      // 自定义热键的作用域
+      scope: "global",
+      // 是否使用事件捕获阶段, true 为使用, 默认 false, 冒泡阶段
+      capture: false,
+      // 是否在按键释放时触发
+      keyup: false,
+      // 是否在按键按下时触发
+      keydown: true,
+      // 定义组合键的分隔符
+      splitKey: "+",
+      // 是否只响应单键（非组合键）
+      single: false,
+    },
+    (event, handler) => {
+      event.preventDefault()
 
-    if (!isMoutend) {
-      isMoutend = true
-      root = onMountedPanel(id, children)
-    } else {
-      isMoutend = false
-      if (root) onUnmountedPanel(id, root)
-      root = null
-    }
+      if (handler.scope === "global") {
+        if (!isMoutend) {
+          isMoutend = true
+          root = onMountedPanel(id, children)
+        } else {
+          isMoutend = false
+          if (root) onUnmountedPanel(id, root)
+          root = null
+        }
 
-    callback?.(isMoutend)
-  })
+        callback?.(isMoutend)
+      }
+    },
+  )
 }
