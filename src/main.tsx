3a4bdf8384f1714b2e34eb0e189792e47503bc35
MiniOcean404 <1037306928@qq.com>
2025-11-20 23:17:16 +0800

    💄 style: 删除无用代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { GM_addElement, GM_log, GM_registerMenuCommand } from "$"
-import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { logger } from "./utils/logger"
-import { onMountedHotkeys, onMountedPanel } from "./pancel"
+import { onMountedHotkeys } from "./pancel"
 
 onMountedHotkeys("vite-monkey-app", "ctrl+shift+u", <App />)
