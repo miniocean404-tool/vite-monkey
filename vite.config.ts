27429e8b647ec73dced5c549e879276d1f28f943
MiniOcean404 <1037306928@qq.com>
2025-12-02 22:41:13 +0800

    ✨ feat: 添加 babel 支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite"
 import monkey from "vite-plugin-monkey"
 import pkg from "./package.json"
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
